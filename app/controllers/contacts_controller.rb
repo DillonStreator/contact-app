189221830d054c1d16436ced81a8f9a05b908783
Dillon Streator <dillonstreator@gmail.com>
2017-03-16 19:18:36 -0500

    Add search bar and fancy display

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,9 @@ class ContactsController < ApplicationController
   def home
     render "home.html.erb"
   end
+  def help
+    flash[:notice] = "Contact was successfully updated!"
+  end
   def index
     @contacts = Contact.all.sort_by {|a| a.last_name}
     render "index.html.erb"
@@ -12,8 +15,10 @@ class ContactsController < ApplicationController
   end
 
   def create
-    Contact.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
-    render "create.html.erb"
+    contact = Contact.new(first_name: params[:first_name].capitalize, last_name: params[:last_name].capitalize, email: params[:email], phone_number: params[:phone_number], middle_name: params[:middle_name], bio: params[:bio])
+    contact.save
+    flash[:success] = "Contact was successfully created!"
+    redirect_to "/contacts/#{contact.id}"
   end
 
   def show
@@ -28,17 +33,27 @@ class ContactsController < ApplicationController
 
   def update
     contact = Contact.find_by(id: params[:id])
-    contact.first_name = params[:first_name]
-    contact.last_name = params[:last_name]
+    contact.first_name = params[:first_name].capitalize
+    contact.middle_name = params[:middle_name].capitalize
+    contact.last_name = params[:last_name].capitalize
     contact.email = params[:email]
     contact.phone_number = params[:phone_number]
+    contact.bio = params[:bio]
     contact.save
-    render "update.html.erb"
+    flash[:success] = "Contact was successfully updated!"
+    redirect_to "/contacts/#{contact.id}"
   end
 
   def destroy
     contact = Contact.find_by(id: params[:id])
     contact.destroy
-    render "destroy.html.erb"
+    flash[:danger] = "Contact was successfully destroyed!"
+    redirect_to "/contacts"
+  end
+
+  def search
+    @search_name = params[:search_name].capitalize
+    @contacts = Contact.all
+    render "search.html.erb"
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,24 @@
 class Contact < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def full_name
+    "#{first_name} #{middle_name} #{last_name}"
+  end
+
+  def japanese_number
+    "+81 #{phone_number}"
+  end
+
+  def self.search(name)
+    contacts_last_name = Contact.where(last_name: name)
+    contacts_first_name = Contact.where(first_name: name)
+    if contacts_first_name.size == 0 or contacts_first_name.size == nil
+      return contacts_last_name
+    else
+      return contacts_first_name
+    end
+  end
+
 end
